6c56dc18eb5d9047561253fb904cf6823961610a
JanardhanBS-SyncByte <janjonny@gmail.com>
2024-11-14 10:11:13 +0530

    [MOSIP-37358] release to develop
    Signed-off-by: JanardhanBS-SyncByte <janjonny@gmail.com>

--- kernel-bio-converter/src/test/java/io/mosip/kernel/bio/converter/ExceptionTest.java
+++ kernel-bio-converter/src/test/java/io/mosip/kernel/bio/converter/ExceptionTest.java
@@ -8,8 +8,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
