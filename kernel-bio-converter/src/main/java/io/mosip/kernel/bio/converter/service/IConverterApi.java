145e58594080bf284b111fce9ffa057ffe266adf
kameshsr <kameshsr1338@gmail.com>
2026-06-04 12:54:14 +0530

    docs: complete IConverterApi Javadoc with full MOS-CNV error code listing
    Previously only MOS-CNV-001, -002, -003, and -500 were listed.
    Added MOS-CNV-004 through MOS-CNV-012 with descriptions matching
    ConverterErrorCode enum entries, as flagged by coderabbitai.
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel-bio-converter/src/main/java/io/mosip/kernel/bio/converter/service/IConverterApi.java
+++ kernel-bio-converter/src/main/java/io/mosip/kernel/bio/converter/service/IConverterApi.java
@@ -39,10 +39,19 @@ public interface IConverterApi {
 	  * @throws ConversionException
 	  *
 	  * ErrorCodes:
-	  * MOS-CNV-001 Conversion source format not supported
-	  * MOS-CNV-002 Conversion target format not supported
-	  * MOS-CNV-003 Invalid source value
-	  * MOS-CNV-500 Technical Error
+	  * MOS-CNV-001 Input source exception (conversion source format not supported)
+	  * MOS-CNV-002 Invalid request value
+	  * MOS-CNV-003 Invalid source exception (invalid source format)
+	  * MOS-CNV-004 Invalid target exception (invalid target format)
+	  * MOS-CNV-005 Source value can not be empty or null
+	  * MOS-CNV-006 Source not valid base64urlencoded
+	  * MOS-CNV-007 Could not read Source ISO Image Data
+	  * MOS-CNV-008 Source not valid ISO ISO19794_4_2011 (finger)
+	  * MOS-CNV-009 Source not valid ISO ISO19794_5_2011 (face)
+	  * MOS-CNV-010 Source not valid ISO ISO19794_6_2011 (iris)
+	  * MOS-CNV-011 Target format exception (conversion to target format failed)
+	  * MOS-CNV-012 Not Supported Compression Type
+	  * MOS-CNV-500 Technical Error (unexpected exception)
 	  */
 	  Map<String, String> convert(Map<String, String> values, String sourceFormat, String targetFormat, Map<String, String> sourceParameters, Map<String, String> targetParameters) throws ConversionException;	  
 	}
